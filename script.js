15c57dee79ec3219b07bf688e56715a5e3be293c
Reno-sanz <78826204+Reno-sanz@users.noreply.github.com>
2021-02-09 16:20:20 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+let montoCofla = prompt("cuanto dinero tienes Cofla?")
+let montoHumberto = prompt("cuanto dinero tienes Humberto?")
+let montoPedro = prompt("cuanto dinero tienes Pedro?")
+
+//monto de cofla
+
+if (montoCofla >= 0.6 && montoCofla < 1) {
+	alert("Cofla comprate el helado de menta")
+	alert("y te sobran" + montoCofla - 0.6)
+}
+if (montoCofla >= 1  && montoCofla < 1.8) {
+	alert("Cofla comprate el helado de mix")
+	alert("y te sobran" + montoCofla - 1)
+}
+if (montoCofla >= 1.8 && montoCofla < 2.9) {
+	alert("Cofla comprate el helado de xd")
+	alert("y te sobran" + montoCofla - 1.8)
+}
+if (montoCofla >= 2.9) {
+	alert("Cofla comprate un litro de helado")
+	alert("y te sobran" + montoCofla - 2.9)
+}
+
+//monto de Humberto
+
+if (montoHumberto >= 0.6 && montoHumberto < 1) {
+	alert("Humberto comprate el helado de menta")
+	alert("y te sobran" + montoHumberto - 0.6)
+}
+if (montoHumberto >= 1  && montoHumberto < 1.8) {
+	alert("Humberto comprate el helado de mix")
+	alert("y te sobran" + montoHumberto - 1)
+}
+if (montoHumberto >= 1.8 && montoHumberto < 2.9) {
+	alert("Humberto comprate el helado de xd")
+	alert("y te sobran" + montoHumberto - 1.8)
+}
+if (montoHumberto >= 2.9) {
+	alert("Humberto comprate un litro de helado")
+	alert("y te sobran" + montoHumberto - 2.9)
+}
+
+//monto de Pedro
+
+if (montoPedro >= 0.6 && montoPedro < 1) {
+	alert("Pedro comprate el helado de menta")
+	alert("y te sobran" + montoPedro - 0.6)
+}
+if (montoPedro >= 1  && montoPedro < 1.8) {
+	alert("Pedro comprate el helado de mix")
+	alert("y te sobran" + montoPedro - 1)
+}
+if (montoPedro >= 1.8 && montoPedro < 2.9) {
+	alert("Pedro comprate el helado de xd")
+	alert("y te sobran" + montoPedro - 1.8) 
+}
+if (montoPedro >= 2.9) {
+	alert("Pedro comprate un litro de helado")
+	alert("y te sobran" + montoPedro - 2.9)
+}
